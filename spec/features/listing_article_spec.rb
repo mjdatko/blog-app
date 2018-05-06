6819b811e91938b7b396a5f2eaf7706e9ce92961
mjdatko <mdatko@sbcglobal.net>
2018-05-06 14:49:00 +0000

    handle article exception not found

---
+++ spec/features/listing_article_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+RSpec.feature "Listing Articles" do
+  before do
+    @article1 = Article.create(title: "First Article", body: "This is the first article body.")
+    @article2 = Article.create(title: "Second Article", body: "This is the second article body.")
+  end
+  
+  scenario "A user list all articles" do
+    visit "/"
+    
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+    
+  end
+  
+  scenario "A user has no articles" do
+    Article.delete_all
+    
+    visit "/"
+    
+    expect(page).not_to have_content(@article1.title)
+    expect(page).not_to have_content(@article1.body)
+    expect(page).not_to have_content(@article2.title)
+    expect(page).not_to have_content(@article2.body)
+    expect(page).not_to have_link(@article1.title)
+    expect(page).not_to have_link(@article2.title)
+    
+    within("h1#no-articles") do
+      expect(page).to have_content("No Articles Created")
+    end
+    
+  end
+end
