b755347c33682c2598cdacedfadd1de03e5db983
mjdatko <mdatko@sbcglobal.net>
2018-05-18 14:18:19 +0000

    Restricted edit acccess at controller

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -2,21 +2,42 @@ require "rails_helper"
 
 RSpec.feature "Listing Articles" do
   before do
-    john = User.create(email: "john@example.com", password: "password")
-    @article1 = Article.create(title: "First Article", body: "This is the first article body.", user: john)
-    @article2 = Article.create(title: "Second Article", body: "This is the second article body.", user: john)
+    @john = User.create(email: "john@example.com", password: "password")
+    @article1 = Article.create(title: "First Article", body: "This is the first article body.", user: @john)
+    @article2 = Article.create(title: "Second Article", body: "This is the second article body.", user: @john)
   end
   
-  scenario "A user list all articles" do
-    visit "/"
-    
+  scenario "With articles created and user not signed in" do
+    visit "/"  
     expect(page).to have_content(@article1.title)
     expect(page).to have_content(@article1.body)
     expect(page).to have_content(@article2.title)
     expect(page).to have_content(@article2.body)
     expect(page).to have_link(@article1.title)
-    expect(page).to have_link(@article2.title)
-    
+    expect(page).to have_link(@article2.title) 
+    expect(page).not_to have_link("New Article")
+  end
+  
+   scenario "With articles created and user signed in" do
+   login_as(@john)
+    visit "/"  
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title) 
+    expect(page).to have_link("New Article")
+  end
+  
+  scenario "A user list all articles" do
+    visit "/"  
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title) 
   end
   
   scenario "A user has no articles" do
